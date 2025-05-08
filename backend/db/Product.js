4249a2772de056230af606d026a48eb61b80f6af
Shivam Yadav <yadavshivamc093@gmail.com>
2025-05-08 23:36:30 +0530

    Updated product schema

--- backend/db/Product.js
+++ backend/db/Product.js
@@ -1,11 +1,12 @@
 // Import mongoose
 const mongoose = require("mongoose");
 
+// Define the Product schema
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true }, // Just the filename, not full URL
+  image: { type: String, required: true }, // Path to the image
 });
 
 module.exports = mongoose.model("Product", productSchema);
